0e6b65e12dfae3590a5eb9e947b40f163b764321
Rachel Ruderman <rachel@yembo.ai>
2019-03-10 23:25:59 -0700

    Use variables with GQL query

--- src/components/Launches/Launch.js
+++ src/components/Launches/Launch.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import {Query} from'react-apollo';
 
-const LAUNCHES_PAST_QUERY = gql`
-    query launchesPast {
-        launchesPast(limit: 2) {
+const LAUNCH_QUERY = gql`
+    query launch($id: ID!) {
+        launch(id: $id) {
             mission_name
             launch_date_local
-        }
+            id
+            details
+      }
     }
 `;
 
 class Launch extends Component {
     render() {
-        return (
-            <div>
-                hey
-                {/*<header className="App-header">*/}
+        const {id} = this.props.match.params;
 
-                {/*</header>*/}
-            </div>
+        return (
+            <Query query={LAUNCH_QUERY} variables={{id}}>
+                { ({data, loading, error}) => {
+                    if (loading)    return 'Loading...';
+                    if (error)      return 'Error';
+
+                    const {launch} = data;
+                    return <h1>{launch.mission_name}</h1>
+                }}
+            </Query>
         );
     }
 }
